8b58e4f082906550a26308d96711ecb54aeb0368
steevened <alvaradosteven315@gmail.com>
2024-06-16 23:40:07 -0500

    fix redirect url when user is auth middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,17 @@
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const authSession = request.cookies.get('auth_session')?.value
+  const authSession = request.cookies.get("auth_session")?.value;
 
-  if (authSession && !request.nextUrl.pathname.startsWith('/dashboard')) {
-    return Response.redirect(new URL('./dashboard', request.url))
+  if (authSession && !request.nextUrl.pathname.startsWith("/dashboard")) {
+    return Response.redirect(new URL("/dashboard", request.url));
   }
 
-  if (!authSession && !request.nextUrl.pathname.startsWith('/login')) {
-    return Response.redirect(new URL('/login', request.url))
+  if (!authSession && !request.nextUrl.pathname.startsWith("/login")) {
+    return Response.redirect(new URL("/login", request.url));
   }
-
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*',],
-}
+  matcher: ["/dashboard/:path*"],
+};
